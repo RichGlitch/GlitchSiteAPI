134cf273551f5e1f9ac18a7e3c61b93b85cdaf26
Ricardo Velasco Salazar <ricardo.pro@outlook.com>
2020-01-03 00:41:58 -0600

    User auth

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using GlitchSiteAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlitchSiteAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
